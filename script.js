8ba3a6427ebee4333fc36761c05a2b4a57e9a922
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-02-13 10:42:27 -0800

    style: add general functionality pseudocode

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-// ----------------------- Calculator JavaScript -------------------------------
+// ------------------------ Calculator JavaScript ------------------------------
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Pseudocode ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /* 
-
-
+- Contain functions for all the basic math operators
+    - add, subtract, multiply, divide
+    - Should all of these functions be on the same object?
+- Calculation
+    - Components: [a] [operator] [b]
+    - Have variable for each component; will be displayed on the calculator
+- Operate function
+    - Calls the appropriate calculation function (based on the input)
+- Inputs from the HTML
+    - Number input from the calculator stored as arguments
+    - Store the operator
+    - Operate when '=' is pressed
+- Order of operations
+    - Automatically calculate previoius subtotal once a new operator is pressed
+    - Operate multiplication/division before addition/subtraction
+        - If multiplication is pressed, do current current operation before 
+          doing addition/subtraction
 
 
 */
 
 "use strict";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Execution ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Testing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
